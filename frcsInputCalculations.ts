3a083e0f0dd29f61a7645aba27d88185cb53b112
Laura Holstege <laholstege@ucdavis.edu>
2020-01-23 15:34:03 -0800

    update f3 to frcs equations

--- frcsInputCalculations.ts
+++ frcsInputCalculations.ts
@@ -1,149 +1,157 @@
-import { Pixel } from './models/pixel';
+import { InputVarMod } from '@ucdavis/frcs/out/systems/frcs.model';
 import { TreatedCluster } from './models/treatedcluster';
 
-export const sumNumberOfTrees = (pixel: Pixel) => {
-  return (
-    // pixel.sng_0 +
-    // pixel.sng_2 +
-    // pixel.sng_7 +
-    // pixel.sng_15 +
-    // pixel.sng_25 +
-    // pixel.sng_35 +
-    // pixel.sng_40 +
-    pixel.tpa_0 + pixel.tpa_2 + pixel.tpa_7 + pixel.tpa_15 + pixel.tpa_25
-    // pixel.tpa_35
-    // pixel.tpa_40
-  );
-};
-
-export const sumBiomass = (pixel: Pixel) => {
-  return (
-    pixel.bmfol_0 +
-    pixel.bmfol_2 +
-    pixel.bmfol_7 +
-    pixel.bmfol_15 +
-    pixel.bmfol_25 +
-    // pixel.bmfol_35 +
-    // pixel.bmfol_40 +
-    pixel.bmcwn_0 +
-    pixel.bmcwn_2 +
-    pixel.bmcwn_7 +
-    pixel.bmcwn_15 +
-    pixel.bmcwn_25 +
-    // pixel.bmcwn_35 +
-    // pixel.bmcwn_40 +
-    pixel.bmstm_0 +
-    pixel.bmstm_2 +
-    pixel.bmstm_7 +
-    pixel.bmstm_15 +
-    pixel.bmstm_25
-    // + pixel.bmstm_35 +
-    // pixel.bmstm_40
-  );
-};
-const metersToAcresConstant = 0.00024711;
-
-const pixelsToAcreConstant = 30 * 30 * metersToAcresConstant;
+export const metersToAcresConstant = 0.00024711;
+export const pixelAreaInAcres = 30 * 30 * metersToAcresConstant;
 
 // these equations come from this sheet:
 // https://ucdavis.app.box.com/file/566320916282
 
-// chip trees removed
-export const calcRemovalsCT = (pixel: Pixel | TreatedCluster) => {
-  // live trees per acre + snags per acre
-  return pixel.tpa_0 + pixel.tpa_2 + pixel.tpa_7;
-  // TODO: add when we get dead biomass  + pixel.sng_0 + pixel.sng_2 + pixel.sng_7);
-};
+export const getFrcsInputs = (cluster: TreatedCluster) => {
+  const weightCT = calcWeightCT(cluster);
+  const volumeCT = calcVolumeCT(cluster);
+  const removalsCT = calcRemovalsCT(cluster);
+  const totalRemovalsCT = calcTotalRemovalsCT(cluster);
 
-export const calcRemovalsSLT = (pixel: Pixel | TreatedCluster) => {
-  return pixel.tpa_15;
-  // + pixel.sng_15) * 1;
-};
+  const weightSLT = calcWeightSLT(cluster);
+  const volumeSLT = calcVolumeSLT(cluster);
+  const removalsSLT = calcRemovalsSLT(cluster);
+  const totalRemovalsSLT = calcTotalRemovalsSLT(cluster);
 
-export const calcRemovalsLLT = (pixel: Pixel | TreatedCluster) => {
-  return pixel.tpa_25;
-  // + pixel.tpa_35
-  // + pixel.tpa_40
-  // + pixel.sng_25 + pixel.sng_35 + pixel.sng_40) * 1
-};
+  const weightLLT = calcWeightLLT(cluster);
+  const volumeLLT = calcVolumeLLT(cluster);
+  const removalsLLT = calcRemovalsLLT(cluster);
+  const totalRemovalsLLT = calcTotalRemovalsLLT(cluster);
 
-export const calcTreeVolCT = (pixel: Pixel | TreatedCluster) => {
-  return (
-    pixel.bmfol_0 +
-    pixel.bmfol_2 +
-    pixel.bmfol_7 +
-    pixel.bmcwn_0 +
-    pixel.bmcwn_2 +
-    pixel.bmcwn_7 +
-    pixel.bmstm_0 +
-    pixel.bmstm_2 +
-    pixel.bmstm_7
-    // TODO: add DBMCN and DBMSM
-  );
-};
-
-export const calcTreeVolSLT = (pixel: Pixel | TreatedCluster) => {
-  return (
-    pixel.bmfol_15 + pixel.bmcwn_15 + pixel.bmstm_15
-    // TODO: add DBMCN 15 and DBMSM 15
-  );
-};
-
-export const calcTreeVolLLT = (pixel: Pixel | TreatedCluster) => {
-  return (
-    pixel.bmfol_25 +
-    // pixel.bmfol_35 +
-    // pixel.bmfol_40 +
-    pixel.bmcwn_25 +
-    // pixel.bmcwn_35 +
-    // pixel.bmcwn_40 +
-    pixel.bmstm_25
-    // pixel.bmstm_35
-    // pixel.bmstm_40
-    // TODO: add DBMCN and DBMSM
-  );
-};
-
-export const sumPixel = (pixelSummation: Pixel, p: Pixel) => {
-  const pixelSum: Pixel = {
-    ...pixelSummation,
-    cluster_no: p.cluster_no,
-    bmcwn_0: pixelSummation.bmcwn_0 + p.bmcwn_0,
-    bmcwn_15: pixelSummation.bmcwn_15 + p.bmcwn_15,
-    bmcwn_2: pixelSummation.bmcwn_2 + p.bmcwn_2,
-    bmcwn_25: pixelSummation.bmcwn_25 + p.bmcwn_25,
-    bmcwn_35: pixelSummation.bmcwn_35 + p.bmcwn_35,
-    bmcwn_40: pixelSummation.bmcwn_40 + p.bmcwn_40,
-    bmcwn_7: pixelSummation.bmcwn_7 + p.bmcwn_7,
-    bmfol_0: pixelSummation.bmfol_0 + p.bmfol_0,
-    bmfol_15: pixelSummation.bmfol_15 + p.bmfol_15,
-    bmfol_2: pixelSummation.bmfol_2 + p.bmfol_2,
-    bmfol_25: pixelSummation.bmfol_25 + p.bmfol_25,
-    bmfol_35: pixelSummation.bmfol_35 + p.bmfol_35,
-    bmfol_40: pixelSummation.bmfol_40 + p.bmfol_40,
-    bmfol_7: pixelSummation.bmfol_7 + p.bmfol_7,
-    bmstm_0: pixelSummation.bmstm_0 + p.bmstm_0,
-    bmstm_15: pixelSummation.bmstm_15 + p.bmstm_15,
-    bmstm_2: pixelSummation.bmstm_2 + p.bmstm_2,
-    bmstm_25: pixelSummation.bmstm_25 + p.bmstm_25,
-    bmstm_35: pixelSummation.bmstm_35 + p.bmstm_35,
-    bmstm_40: pixelSummation.bmstm_40 + p.bmstm_40,
-    bmstm_7: pixelSummation.bmstm_7 + p.bmstm_7,
-    sng_0: pixelSummation.sng_0 + p.sng_0,
-    sng_15: pixelSummation.sng_15 + p.sng_15,
-    sng_2: pixelSummation.sng_2 + p.sng_2,
-    sng_25: pixelSummation.sng_25 + p.sng_25,
-    sng_35: pixelSummation.sng_35 + p.sng_35,
-    sng_40: pixelSummation.sng_40 + p.sng_40,
-    sng_7: pixelSummation.sng_7 + p.sng_7,
-    // get # of trees per pixel
-    tpa_0: pixelSummation.tpa_0 + p.tpa_0, // * pixelsToAcreConstant,
-    tpa_15: pixelSummation.tpa_15 + p.tpa_15, // * pixelsToAcreConstant,
-    tpa_2: pixelSummation.tpa_2 + p.tpa_2, // * pixelsToAcreConstant,
-    tpa_25: pixelSummation.tpa_25 + p.tpa_25, // * pixelsToAcreConstant,
-    tpa_35: pixelSummation.tpa_35 + p.tpa_35, // * pixelsToAcreConstant,
-    tpa_40: pixelSummation.tpa_40 + p.tpa_40, // * pixelsToAcreConstant,
-    tpa_7: pixelSummation.tpa_7 + p.tpa_7 // * pixelsToAcreConstant
+  const frcsInputs: InputVarMod = {
+    System: 'Ground-Based Mech WT',
+    PartialCut: true,
+    DeliverDist: cluster.total_yarding,
+    Slope: cluster.slope,
+    Elevation: cluster.center_elevation,
+    CalcLoad: true,
+    CalcMoveIn: true,
+    Area: cluster.area,
+    MoveInDist: 2,
+    CalcResidues: true,
+    UserSpecWDCT: weightCT / volumeCT,
+    UserSpecWDSLT: weightSLT / volumeSLT,
+    UserSpecWDLLT: weightLLT / volumeLLT,
+    UserSpecRFCT: 0,
+    UserSpecRFSLT: 0.25,
+    UserSpecRFLLT: 0.38,
+    UserSpecHFCT: 0.2,
+    UserSpecHFSLT: 0,
+    UserSpecHFLLT: 0,
+    RemovalsCT: removalsCT,
+    RemovalsSLT: removalsSLT,
+    RemovalsLLT: removalsLLT,
+    TreeVolCT: volumeCT / totalRemovalsCT,
+    TreeVolSLT: volumeSLT / totalRemovalsSLT,
+    TreeVolLLT: volumeLLT / totalRemovalsLLT,
+    DieselFuelPrice: 3.882
   };
-  return pixelSum;
+  return frcsInputs;
+};
+
+const calcWeightCT = (cluster: TreatedCluster) => {
+  return (
+    2000 *
+    (cluster.bmfol_0 + // U.S. tons
+      cluster.bmfol_2 +
+      cluster.bmfol_7 +
+      cluster.bmcwn_0 +
+      cluster.bmcwn_2 +
+      cluster.bmcwn_7 +
+      cluster.bmstm_0 +
+      cluster.bmstm_2 +
+      cluster.bmstm_7)
+  ); // TODO: add dbmcn and dbmsm
+};
+
+const calcVolumeCT = (cluster: TreatedCluster) => {
+  return calculateVolume(cluster, 0) + calculateVolume(cluster, 2) + calculateVolume(cluster, 7);
+};
+
+const calcRemovalsCT = (cluster: TreatedCluster) => {
+  return (cluster.tpa_0 + cluster.tpa_2 + cluster.tpa_7) / (cluster.area / pixelAreaInAcres); // TODO: add snags
+};
+
+const calcTotalRemovalsCT = (cluster: TreatedCluster) => {
+  return pixelAreaInAcres * (cluster.tpa_0 + cluster.tpa_2 + cluster.tpa_7); // TODO: add snags
+};
+
+const calcWeightSLT = (cluster: TreatedCluster) => {
+  return (
+    2000 *
+    (cluster.bmfol_15 + // U.S. tons
+      cluster.bmcwn_15 +
+      cluster.bmstm_15)
+  ); // TODO: add dbmcn and dbmsm
+};
+
+const calcVolumeSLT = (cluster: TreatedCluster) => {
+  return calculateVolume(cluster, 15);
+};
+
+const calcRemovalsSLT = (cluster: TreatedCluster) => {
+  return cluster.tpa_15 / (cluster.area / pixelAreaInAcres); // TODO: add snags
+};
+
+const calcTotalRemovalsSLT = (cluster: TreatedCluster) => {
+  return pixelAreaInAcres * cluster.tpa_15; // TODO: add snags
+};
+
+const calcWeightLLT = (cluster: TreatedCluster) => {
+  return (
+    2000 *
+    (cluster.bmfol_25 + // U.S. tons
+      cluster.bmcwn_25 +
+      cluster.bmstm_25)
+  ); // TODO: add dbmcn and dbmsm
+};
+
+const calcVolumeLLT = (cluster: TreatedCluster) => {
+  return calculateVolume(cluster, 25);
+};
+
+const calcRemovalsLLT = (cluster: TreatedCluster) => {
+  return cluster.tpa_25 / (cluster.area / pixelAreaInAcres); // TODO: add snags
+};
+
+const calcTotalRemovalsLLT = (cluster: TreatedCluster) => {
+  return pixelAreaInAcres * cluster.tpa_25; // TODO: add snags
+};
+
+const calculateVolume = (cluster: TreatedCluster, i: number) => {
+  let vol = 0;
+  let avgDBH = 0;
+  switch (i) {
+    // for dbh < 5, use equation from here: https://ucdavis.app.box.com/file/602500273957
+    case 0:
+      avgDBH = 0.5;
+      vol = cluster.tpa_0 * pixelAreaInAcres * (avgDBH * 1.7925);
+      break;
+    case 2:
+      avgDBH = 3;
+      vol = cluster.tpa_2 * pixelAreaInAcres * (avgDBH * 1.7925);
+      break;
+    // otherwise use this equation https://ucdavis.app.box.com/file/566320916282
+    case 7:
+      avgDBH = 7.5;
+      // trees/acre in cluster * pixel area * cubic feet per tree
+      vol = cluster.tpa_7 * pixelAreaInAcres * (avgDBH * avgDBH * 0.216 - 3.675);
+      break;
+    case 15:
+      avgDBH = 15;
+      // trees/acre in cluster * pixel area * cubic feet per tree
+      vol = cluster.tpa_15 * pixelAreaInAcres * (avgDBH * avgDBH * 0.216 - 3.675);
+      break;
+    case 25:
+      avgDBH = 25;
+      // trees/acre in cluster * pixel area * cubic feet per tree
+      vol = cluster.tpa_25 * pixelAreaInAcres * (avgDBH * avgDBH * 0.216 - 3.675);
+      break;
+  }
+  return vol;
 };
