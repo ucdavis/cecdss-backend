4aab8258e7b7c31e82dded170700a7608b70c0c0
Laura Holstege <laholstege@ucdavis.edu>
2020-02-07 15:55:38 -0800

    CORS prod

--- index.ts
+++ index.ts
@@ -33,9 +33,9 @@ const db = knex({
   }
 });
 
-// allow cors for local dev
+// allow cors
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'https://cecdss.azurewebsites.net/');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
