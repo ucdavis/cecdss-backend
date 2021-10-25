475d4a3020723d4eb65cb7bb14c31b4b4535dcec
Scott Kirkland <srkirkland@gmail.com>
2021-10-25 10:20:45 -0700

    calculate and return distance center->landing

--- models/types.ts
+++ models/types.ts
@@ -192,6 +192,7 @@ export interface ClusterResult {
   center_lng: number;
   landing_lat: number;
   landing_lng: number;
+  landing_distance: number;
   county: string;
   land_use: string;
   site_class: number;
