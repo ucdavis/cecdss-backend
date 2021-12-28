48f0d585556c5b5139e6f5df6f5e01a67e160403
Scott Kirkland <srkirkland@gmail.com>
2021-12-28 10:52:53 -0800

    set 5 min timeout and wrap methods in try/catch
    this way instead of our callers getting nothing back on errors they'll get back 500 status codes.
    
    Can be augmented in the future to send different status codes depending on the type of error if we so desire.

--- index.ts
+++ index.ts
@@ -43,6 +43,8 @@ if (process.env.APPINSIGHTS_INSTRUMENTATIONKEY) {
   setupAppInsights();
 }
 
+const apiTimeout = 60 * 5 * 1000; // 5 minutes
+
 const app = express();
 
 app.use(bodyParser.json({ limit: '50mb' }));
@@ -74,6 +76,16 @@ console.log('connected to osrm');
 // allow cors
 app.use(cors());
 
+// 5 minutes to timeout, extending the default of 2 minutes
+app.use((req, res, next) => {
+  res.setTimeout(apiTimeout, () => {
+    console.log('Request has timed out.');
+    res.send(500);
+  });
+
+  next();
+});
+
 // constants
 const systems = [
   'Ground-Based Mech WT',
@@ -91,77 +103,82 @@ const systems = [
 app.post('/initialProcessing', async (req, res) => {
   console.log('running initial processing...');
 
-  const params: RequestParamsAllYears = req.body;
-  const bounds = getBoundsOfDistance(
-    { latitude: params.facilityLat, longitude: params.facilityLng },
-    150000
-  );
-  // TODO: clean up, add substation type
-  const substations: any[] = await db
-    .table('substations') // TODO: only select appropriate voltage
-    .whereBetween('latitude', [bounds[0].latitude, bounds[1].latitude])
-    .andWhereBetween('longitude', [bounds[0].longitude, bounds[1].longitude]);
-  console.log(`found: ${substations.length} substations`);
-  const nearestSubstation: any =
-    substations.length > 1
-      ? findNearest(
-          { latitude: params.facilityLat, longitude: params.facilityLng },
-          substations.map((substation) => {
-            return { latitude: substation.latitude, longitude: substation.longitude };
-          })
-        )
-      : substations[0];
-  const distanceToNearestSubstationInM = getDistance(
-    { latitude: params.facilityLat, longitude: params.facilityLng },
-    { latitude: nearestSubstation.latitude, longitude: nearestSubstation.longitude }
-  ); // m
-  const distanceToNearestSubstation = (distanceToNearestSubstationInM / 1000) * KM_TO_MILES;
-  const distanceAveraged = distanceToNearestSubstation / 8; // 8 types of land,
-  if (distanceToNearestSubstation < 3) {
-    params.transmission.LengthCategory = '< 3 miles';
-  } else if (distanceToNearestSubstation >= 3 && distanceToNearestSubstation <= 10) {
-    params.transmission.LengthCategory = '3-10 miles';
-  } else {
-    params.transmission.LengthCategory = '> 10 miles';
+  try {
+    const params: RequestParamsAllYears = req.body;
+    const bounds = getBoundsOfDistance(
+      { latitude: params.facilityLat, longitude: params.facilityLng },
+      150000
+    );
+    // TODO: clean up, add substation type
+    const substations: any[] = await db
+      .table('substations') // TODO: only select appropriate voltage
+      .whereBetween('latitude', [bounds[0].latitude, bounds[1].latitude])
+      .andWhereBetween('longitude', [bounds[0].longitude, bounds[1].longitude]);
+    console.log(`found: ${substations.length} substations`);
+    const nearestSubstation: any =
+      substations.length > 1
+        ? findNearest(
+            { latitude: params.facilityLat, longitude: params.facilityLng },
+            substations.map((substation) => {
+              return { latitude: substation.latitude, longitude: substation.longitude };
+            })
+          )
+        : substations[0];
+    const distanceToNearestSubstationInM = getDistance(
+      { latitude: params.facilityLat, longitude: params.facilityLng },
+      { latitude: nearestSubstation.latitude, longitude: nearestSubstation.longitude }
+    ); // m
+    const distanceToNearestSubstation = (distanceToNearestSubstationInM / 1000) * KM_TO_MILES;
+    const distanceAveraged = distanceToNearestSubstation / 8; // 8 types of land,
+    if (distanceToNearestSubstation < 3) {
+      params.transmission.LengthCategory = '< 3 miles';
+    } else if (distanceToNearestSubstation >= 3 && distanceToNearestSubstation <= 10) {
+      params.transmission.LengthCategory = '3-10 miles';
+    } else {
+      params.transmission.LengthCategory = '> 10 miles';
+    }
+    params.transmission.Miles = {
+      ...params.transmission.Miles,
+      Forested: distanceAveraged,
+      Flat: distanceAveraged,
+      Wetland: distanceAveraged,
+      Farmland: distanceAveraged,
+      Desert: distanceAveraged,
+      Urban: distanceAveraged,
+      Hills: distanceAveraged,
+      Mountain: distanceAveraged,
+    };
+    console.log(
+      `nearest substation: ${nearestSubstation.substation_name} is ${distanceToNearestSubstation} miles away`
+    );
+    const transmissionResults = transmission(params.transmission);
+    console.log(`transmission cost: ${transmissionResults.AllCost}`);
+
+    const teaInputs: any = { ...params.teaInputs };
+    teaInputs.CapitalCost += transmissionResults.AllCost;
+    console.log(JSON.stringify(teaInputs));
+    const teaOutput: OutputModGPO | OutputModCHP | OutputModGP = await getTeaOutputs(
+      params.teaModel,
+      teaInputs
+    );
+    const biomassTarget = teaOutput.ElectricalAndFuelBaseYear.BiomassTarget;
+    const electricalAndFuelBaseYear: any = teaOutput.ElectricalAndFuelBaseYear;
+    const annualGeneration = electricalAndFuelBaseYear.AnnualGeneration;
+    const results: AllYearsResults = {
+      biomassTarget: biomassTarget,
+      transmissionResults: transmissionResults,
+      teaResults: teaOutput,
+      teaInputs: teaInputs,
+      annualGeneration: annualGeneration,
+      nearestSubstation: nearestSubstation.substation_name,
+      distanceToNearestSubstation: distanceToNearestSubstationInM / 1000, // km
+    };
+
+    res.status(200).json(results);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
   }
-  params.transmission.Miles = {
-    ...params.transmission.Miles,
-    Forested: distanceAveraged,
-    Flat: distanceAveraged,
-    Wetland: distanceAveraged,
-    Farmland: distanceAveraged,
-    Desert: distanceAveraged,
-    Urban: distanceAveraged,
-    Hills: distanceAveraged,
-    Mountain: distanceAveraged,
-  };
-  console.log(
-    `nearest substation: ${nearestSubstation.substation_name} is ${distanceToNearestSubstation} miles away`
-  );
-  const transmissionResults = transmission(params.transmission);
-  console.log(`transmission cost: ${transmissionResults.AllCost}`);
-
-  const teaInputs: any = { ...params.teaInputs };
-  teaInputs.CapitalCost += transmissionResults.AllCost;
-  console.log(JSON.stringify(teaInputs));
-  const teaOutput: OutputModGPO | OutputModCHP | OutputModGP = await getTeaOutputs(
-    params.teaModel,
-    teaInputs
-  );
-  const biomassTarget = teaOutput.ElectricalAndFuelBaseYear.BiomassTarget;
-  const electricalAndFuelBaseYear: any = teaOutput.ElectricalAndFuelBaseYear;
-  const annualGeneration = electricalAndFuelBaseYear.AnnualGeneration;
-  const results: AllYearsResults = {
-    biomassTarget: biomassTarget,
-    transmissionResults: transmissionResults,
-    teaResults: teaOutput,
-    teaInputs: teaInputs,
-    annualGeneration: annualGeneration,
-    nearestSubstation: nearestSubstation.substation_name,
-    distanceToNearestSubstation: distanceToNearestSubstationInM / 1000, // km
-  };
-
-  res.status(200).json(results);
 });
 
 app.post('/runLCA', async (req, res) => {
@@ -171,8 +188,6 @@ app.post('/runLCA', async (req, res) => {
 });
 
 app.post('/process', async (req, res) => {
-  req.setTimeout(60 * 5 * 1000); // 5 minutes to timeout, extending the default of 2 minutes
-
   const t0 = performance.now();
   const params: RequestParams = req.body;
 
@@ -185,25 +200,31 @@ app.post('/process', async (req, res) => {
     res.status(400).send('TEA Model not recognized');
   }
 
-  const yearResult = await processClustersForYear(
-    db,
-    osrm,
-    params.radius,
-    params,
-    // 10000,
-    params.biomassTarget,
-    params.year,
-    params.clusterIds,
-    params.errorIds
-  );
-  console.log(`year: ${params.year}, # of clusters: ${yearResult.clusterNumbers.length}`);
+  try {
+    const yearResult = await processClustersForYear(
+      db,
+      osrm,
+      params.radius,
+      params,
+      // 10000,
+      params.biomassTarget,
+      params.year,
+      params.clusterIds,
+      params.errorIds
+    );
 
-  const t1 = performance.now();
-  console.log(`Running took ${t1 - t0} milliseconds.`);
+    console.log(`year: ${params.year}, # of clusters: ${yearResult.clusterNumbers.length}`);
 
-  trackMetric(`Process method ${params.year} - biomass target ${params.biomassTarget}`, t1 - t0);
+    const t1 = performance.now();
+    console.log(`Running took ${t1 - t0} milliseconds.`);
 
-  res.status(200).json(yearResult);
+    trackMetric(`Process method ${params.year} - biomass target ${params.biomassTarget}`, t1 - t0);
+
+    res.status(200).json(yearResult);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
 });
 
 app.post('/processDistance', async (req, res) => {
@@ -315,7 +336,8 @@ app.post('/processMoveIn', async (req, res) => {
 });
 
 // tslint:disable-next-line: max-file-line-count
-app.listen(port, () => console.log(`Listening on port ${port}!`));
+const server = app.listen(port, () => console.log(`Listening on port ${port}!`));
+server.setTimeout(apiTimeout);
 
 app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
